5f3767aea5779f9aab4d051e3107551ba0edf5cf
tonicesar81 <tonicaamaral@gmail.com>
2018-11-09 13:32:37 -0200

    Correção

--- app/Http/Controllers/ProjetosController.php
+++ app/Http/Controllers/ProjetosController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Mail;
 use App\Shopping;
 use App\Projetos;
 use App\ProjetosArquivos;
